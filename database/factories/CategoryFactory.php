72b04c83c712f7fd21437581f28fae774c861539
suvosarker6688 <126939552+suvosarker6688@users.noreply.github.com>
2023-05-11 21:53:06 +0600

    data genareted

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->title;
+        return [
+           'title' => $title,
+           'slug' => Str::slug($title),
+           'is_active' => 1
+        ];
+    }
+}
